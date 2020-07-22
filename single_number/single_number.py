ecdbecb372478df280d5d2d8daffbffb0b8a2d60
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-22 15:17:00 -0700

    comments

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -6,17 +6,23 @@ Returns: an integer
 
 def single_number(arr):
     # Your code here
+    # sort array
     arr.sort()
 
+    # define single number and iterator
     single = 0
     i = 0
 
+    # loop over sorted array
     while i < len(arr):
+        # if we've reached the last element in the array, it's the single number
         if i == len(arr) - 1:
             single = arr[i]
             return single
+        # if the number is equal to it's right neighbor, skip the neighbor
         elif arr[i] == arr[i + 1]:
             i += 2
+        # return the single number
         else:
             single = arr[i]
             return single
